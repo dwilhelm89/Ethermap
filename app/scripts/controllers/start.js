5fab9325022f1dfb6c164c23be1868e1c253d5c2
Dennis Wilhelm <dwilhelm89@googlemail.com>
2014-04-12 16:36:54 +0200

    getActions api

--- app/scripts/controllers/start.js
+++ app/scripts/controllers/start.js
@@ -4,7 +4,8 @@ angular.module('CollaborativeMap')
   .controller('StartCtrl', ['$scope', '$rootScope', '$location', 'Utils', function($scope, $rootScope, $location, Utils){
     
     $scope.startClick = function(){
-      var mapId = $scope.mapIdInput || Utils.createId();
+      //a+  as couchdb db names can't start with a number
+      var mapId = $scope.mapIdInput || 'a' + Utils.createId();
       $rootScope.userName = $scope.userInput;
       $location.path('/map/' + mapId);
     };
