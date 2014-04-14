97628c6393767eb24c2555fd0f16c42062f7a6d0
Dennis Wilhelm <dwilhelm89@googlemail.com>
2014-04-14 13:20:49 +0200

    start map with enter

--- app/scripts/controllers/start.js
+++ app/scripts/controllers/start.js
@@ -3,11 +3,21 @@
 angular.module('CollaborativeMap')
   .controller('StartCtrl', ['$scope', '$rootScope', '$location', 'Utils', function($scope, $rootScope, $location, Utils){
     
-    $scope.startClick = function(){
+    function startMap(){
       //a+  as couchdb db names can't start with a number
       var mapId = $scope.mapIdInput || 'a' + Utils.createId();
       $rootScope.userName = $scope.userInput;
       $location.path('/map/' + mapId);
+    }
+
+    $scope.startClick = function(){
+      startMap();
+    };
+
+    $scope.startWithKey = function(e){
+      if(e.keyCode === 13){
+        startMap();
+      }
     };
 
   }]);
