ead72f226f161a79c7cf8d9b75a729eeb78aa242
dwilhelm89 <dwilhelm89@googlemail.com>
2014-04-17 14:58:02 +0200

    minor changes

--- app/scripts/history/historyDirective.js
+++ app/scripts/history/historyDirective.js
@@ -4,17 +4,9 @@ angular.module('CollaborativeMap')
 .directive('history', ['$http', function($http){
 
 	return {
-		// name: '',
-		// priority: 1,
-		// terminal: true,
-		// scope: {}, // {} = isolate, true = child, false/undefined = no change
-		// controller: function($scope, $element, $attrs, $transclude) {},
-		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-		// template: '',
 		templateUrl: 'partials/history',
 		replace: true,
-		// replace: true,
 		// transclude: true,
 		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 		link: function($scope, iElm, iAttrs, controller) {
