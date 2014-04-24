a813c110bcdd8a7bbb95dca202fd2b9a2ee3324d
Dennis Wilhelm <dwilhelm89@googlemail.com>
2014-04-24 09:40:21 +0200

    raven for bugtracking

--- server.js
+++ server.js
@@ -28,12 +28,10 @@ websocketServer.listen(config.port);
 
 console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
 
-/*
-// Start server
-app.listen(config.port, function() {
-  console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
-});
-*/
+//error logging with raven
+var raven = require('raven');
+app.use(raven.middleware.express('https://db4f99d9238948be9d1e2fa85b34084a:6d031ec71a6a41c3bdea826ac9e55db9@app.getsentry.com/22998'));
+
 
 
 // Expose app
