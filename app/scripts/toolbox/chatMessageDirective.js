8bee8a49ab667480352c511f19e100c6b8bc696a
dwilhelm89 <dwilhelm89@googlemail.com>
2014-05-15 14:34:35 +0200

    refer to feature

---
+++ app/scripts/toolbox/chatMessageDirective.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * @memberof CollaborativeMap
+ * @fileOverview Chat directive. Provides the GUI functionality as well as the WebSocket connection for the chat.
+ * @exports CollaborativeMap.chat
+ * @author Dennis Wilhelm
+ */
+angular.module('CollaborativeMap')
+  .directive('chatMessage', ['MapHandler', '$compile',
+    function(MapHandler, $compile) {
+      return {
+        template: '<div></div>',
+        restrict: 'E',
+        scope: {
+          message: '='
+        },
+        link: function postLink(scope, element) {
+
+          function exchangeFid() {
+            var index = message.indexOf('#');
+            if (index > -1) {
+              var fidString = message.substring(index).split(' ')[0];
+              message = createButton(fidString);
+            }
+            if (message.indexOf('#') > -1) {
+              exchangeFid();
+            }
+          }
+
+          function createButton(fidString) {
+            return message.replace(fidString, '<span class="featureInChat" ng-click="panToFeature(' + fidString.substring(1) + ')">Feature</span>');
+          }
+
+          scope.panToFeature = function(fid) {
+            MapHandler.panToFeature(fid);
+            MapHandler.highlightFeatureId(fid);
+          };
+
+          var message = '';
+          if (scope.message && scope.message.message) {
+            message = scope.message.message;
+          }
+
+          exchangeFid(message);
+          element[0].innerHTML = message;
+          var e = angular.element(element[0]);
+          $compile(e.contents())(scope);
+
+        }
+      };
+    }
+  ]);
