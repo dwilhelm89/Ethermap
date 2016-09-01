b4bf0a66321fe895d3a9b9fa26ea67b94d4623f9
noerw <noerw@gmx.de>
2016-09-01 19:49:32 +0200

    improve map movement updates
    use leaflet map events to catch any view updates (adding resize, refer2feature, zoom, ...)

--- app/scripts/leaflet/mapMovementEventsService.js
+++ app/scripts/leaflet/mapMovementEventsService.js
@@ -2,11 +2,11 @@
 
 /**
  * @memberof CollaborativeMap
- * 
- * @fileOverview Listeners for mouse drage and scroll events to catch map movements. 
- * Used instead of direct leaflet events to prevent back coupling problems. 
+ *
+ * @fileOverview Listeners for mouse drage and scroll events to catch map movements.
+ * Used instead of direct leaflet events to prevent back coupling problems.
  * Callbacks of all events are handled in the SynchronizeMapService.
- * 
+ *
  * @exports CollaborativeMap.MapMovementEvents
  * @author Dennis Wilhelm
  */
@@ -20,68 +20,16 @@ angular.module('CollaborativeMap')
        * @param  {Function} callback
        */
       connectMapEvents: function(map, callback) {
-
-        var element = map._container;
-
-        this.addDragEvent(map, element, callback);
-        this.addScrollEvent(map, element, callback);
-
+        // catches any map movements (drag, zoom, resize, ...)
+        map.on('moveend', function(e) {
+          var bounds = map.getBounds();
+          callback({
+            'nE': [bounds._northEast.lat, bounds._northEast.lng],
+            'sW': [bounds._southWest.lat, bounds._southWest.lng]
+          });
+        });
       },
 
-      /**
-       * Adds the mouse listeners. If drag is catched, get the map bounds and fire the callback
-       * @param {Object}   map      the map
-       * @param {Object}   element  html element
-       * @param {Function} callback
-       */
-      addDragEvent: function(map, element, callback) {
-        var flag = 0;
-
-        element.addEventListener('mousedown', function() {
-          flag = 0;
-        }, false);
-        element.addEventListener('mousemove', function() {
-          flag = 1;
-        }, false);
-        element.addEventListener('mouseup', function() {
-          if (flag === 0) {
-            //click
-          } else if (flag === 1) {
-            //If a drag has been performed, upload the new map-position
-            callback(this.getBoundsMessage(map));
-          }
-        }.bind(this), false);
-      },
-
-      /**
-       * Adds the scroll listeners. If scroll is catched, get the map bounds and fire the callback
-       * @param {Object}   map      the map
-       * @param {Object}   element  html element
-       * @param {Function} callback 
-       */
-      addScrollEvent: function(map, element, callback) {
-        element.addEventListener('mousewheel', function() {
-          callback(this.getBoundsMessage(map));
-        }.bind(this), false);
-
-        element.addEventListener('DOMMouseScroll', function() {
-          callback(this.getBoundsMessage(map));
-        }.bind(this), false);
-
-      },
-
-      /**
-       * Get the current bounding box of the map
-       * @param  {Object} map the map
-       * @return {Object}     bounding box {nE, sW}
-       */
-      getBoundsMessage: function(map) {
-        var bounds = map.getBounds();
-        return {
-          'nE': [bounds._northEast.lat, bounds._northEast.lng],
-          'sW': [bounds._southWest.lat, bounds._southWest.lng]
-        };
-      }
 
     };
   });
